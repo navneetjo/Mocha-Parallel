b18bfec6c9953cc9ecd86170eea5f05df3d7aff7
navneetjo <navneetjo@cybage.com>
2015-10-20 17:41:40 +0530

    commit message

--- express.js
+++ express.js
@@ -20,11 +20,12 @@ app.get('/', function(req, res, next) {
 })
 
 app.get('/collections/:collectionName', function(req, res, next) {
-  req.collection.find({} ,{limit: 10, sort: {'_id': -1}}).toArray(function(e, results){
+  req.collection.find({} ,{limit: 20, sort: {'_id': -1}}).toArray(function(e, results){
     if (e) return next(e)
     res.send(results)
   })
 })
+
 
 app.post('/collections/:collectionName', function(req, res, next) {
   req.collection.insert(req.body, {}, function(e, results){
--- express.test.js
+++ express.test.js
@@ -1,13 +1,17 @@
 var superagent = require('superagent')
 var expect = require('expect.js')
+var parallel=require('mocha.parallel');
 
-describe('express rest api server', function(){
+parallel('express rest api server', function(){
   var id
 
-  it('posts an object', function(done){
-    superagent.post('http://localhost:3000/collections/test')
-      .send({ name: 'John'
-        , email: 'john@rpjs.co'
+  it('posts an object',function(done){
+	  
+  setTimeout(function(){
+    superagent.post('http://localhost:3000/collections/employees')
+      .send({ code : '5555',
+	  name: 'Bama'
+        , city: 'pune'
       })
       .end(function(e, res){
         // console.log(res.body)
@@ -15,67 +19,89 @@ describe('express rest api server', function(){
         expect(res.body.length).to.eql(1)
         expect(res.body[0]._id.length).to.eql(24)
         id = res.body[0]._id
+		console.log("POST ID"+id)
+		console.log(res.body)
         done()
+	 },100);
       })
+	   
   })
 
   it('retrieves an object', function(done){
-    superagent.get('http://localhost:3000/collections/test/'+id)
+	  setTimeout(function(){
+    superagent.get('http://localhost:3000/collections/employees/'+id)
       .end(function(e, res){
         // console.log(res.body)
         expect(e).to.eql(null)
         expect(typeof res.body).to.eql('object')
         expect(res.body._id.length).to.eql(24)
         expect(res.body._id).to.eql(id)
-        expect(res.body.name).to.eql('John')
-        done()
-      })
+        expect(res.body.name).to.eql('Bama')
+		//console.log("ID"+id)
+		//console.log(res.body)
+        done();
+      
+	  })
+	  },200);
+	  
   })
 
   it('retrieves a collection', function(done){
-    superagent.get('http://localhost:3000/collections/test')
+	  setTimeout(function(){
+    superagent.get('http://localhost:3000/collections/employees')
       .end(function(e, res){
         // console.log(res.body)
         expect(e).to.eql(null)
         expect(res.body.length).to.be.above(0)
         expect(res.body.map(function (item){return item._id})).to.contain(id)
-        done()
+        done();
       })
+	   },300);
   })
 
   it('updates an object', function(done){
-    superagent.put('http://localhost:3000/collections/test/'+id)
-      .send({name: 'Peter'
-        , email: 'peter@yahoo.com'})
+	  setTimeout(function(){
+    superagent.put('http://localhost:3000/collections/employees/'+id)
+      .send({ code :'8888',
+		  name: 'NewBama'
+        , city: 'mumbai'})
       .end(function(e, res){
         // console.log(res.body)
         expect(e).to.eql(null)
         expect(typeof res.body).to.eql('object')
         expect(res.body.msg).to.eql('success')
-        done()
+        done();
       })
+	  },400);
   })
 
   it('checks an updated object', function(done){
-    superagent.get('http://localhost:3000/collections/test/'+id)
+	   setTimeout(function(){
+    superagent.get('http://localhost:3000/collections/employees/'+id)
       .end(function(e, res){
         // console.log(res.body)
         expect(e).to.eql(null)
         expect(typeof res.body).to.eql('object')
         expect(res.body._id.length).to.eql(24)
         expect(res.body._id).to.eql(id)
-        expect(res.body.name).to.eql('Peter')
-        done()
+        expect(res.body.code).to.eql('8888')
+		expect(res.body.name).to.eql('NewBama')
+		expect(res.body.city).to.eql('mumbai')
+        done();
       })
+	  },500);
   })
   it('removes an object', function(done){
-    superagent.del('http://localhost:3000/collections/test/'+id)
+	   setTimeout(function(){
+    superagent.del('http://localhost:3000/collections/employees/'+id)
       .end(function(e, res){
         // console.log(res.body)
         expect(e).to.eql(null)
         expect(typeof res.body).to.eql('object')
         expect(res.body.msg).to.eql('success')
-        done()
+        done();
       })
+	  },600);
   })
+  
 })
